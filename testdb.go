b191b33b2537bbdaa99aa7e33ce5415c53644c46
makki_d <d.makiuchi@gmail.com>
2023-11-28 21:41:25 +0900

    multi database

--- testdb.go
+++ testdb.go
@@ -3,6 +3,7 @@ package testdb
 import (
 	"database/sql"
 	"os"
+	"strings"
 
 	"github.com/dolthub/go-mysql-server/driver"
 	"github.com/dolthub/go-mysql-server/memory"
@@ -15,13 +16,19 @@ func (d dbs) Resolve(name string, options *driver.Options) (string, sqle.Databas
 	return name, memory.NewDBProvider(d...), nil
 }
 
-func New(dbName string) *sql.DB {
-	memdb := memory.NewDatabase(dbName)
-	memdb.EnablePrimaryKeyIndexes()
-	drv := driver.New(dbs{memdb}, nil)
-	conn := Must1(drv.OpenConnector(dbName))
+func New(dbNames ...string) *sql.DB {
+	var dbs dbs
+	for _, dbName := range dbNames {
+		memdb := memory.NewDatabase(dbName)
+		memdb.EnablePrimaryKeyIndexes()
+		dbs = append(dbs, memdb)
+	}
+	drv := driver.New(dbs, nil)
+	conn := Must1(drv.OpenConnector(strings.Join(dbNames, ";")))
 	db := sql.OpenDB(conn)
-	Must1(db.Exec("USE " + dbName))
+	if len(dbNames) > 0 {
+		Must1(db.Exec("USE " + dbNames[0]))
+	}
 	return db
 }
 
